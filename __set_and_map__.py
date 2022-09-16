d0a02238195e9fd07b83902c509c530ad3a1f9d7
prettygood236 <prettygood236@gmail.com>
2022-09-16 17:12:38 +0900

    Done with all set and map question.! :)

--- __set_and_map__.py
+++ __set_and_map__.py
@@ -1,15 +1,104 @@
+# import sys
+
+# N = int(sys.stdin.readline())
+# Set_a = set(map(int,sys.stdin.readline().split()))
+# M = int(sys.stdin.readline())
+# L = list(map(int,sys.stdin.readline().split()))
+# Set_b = set(L)
+
+# intersection = Set_a & Set_b
+
+# for i in range(M):
+#   if L[i] in intersection:
+#     print(1)
+#   else:
+#     print(0)
+
+
+
+# import sys 
+# N,M = map(int,sys.stdin.readline().split())
+# S = set(sys.stdin.readline().strip() for _ in range(N))
+# count = 0
+
+# for _ in range(M):
+#   temp = sys.stdin.readline().strip()
+#   if temp in S:
+#     count += 1
+
+# print(count)
+
+
+
+# import sys
+# N,M = map(int,sys.stdin.readline().split())
+# D = {}
+
+# for i in range(1,N+1):
+#   temp = sys.stdin.readline().strip()
+#   D[str(i)] = temp
+#   D[temp] = str(i)
+
+# for _ in range(M):
+#   Q = sys.stdin.readline().strip()
+#   print(D[Q])
+
+
+
+# import sys
+# from collections import Counter
+# N = int(sys.stdin.readline())
+# L = map(int,sys.stdin.readline().split())
+# R = []
+
+# cnt = Counter(L)
+# M = int(sys.stdin.readline())
+
+# R = list(map(int,sys.stdin.readline().split()))
+  
+# for i in R:
+#   print(cnt[i])
+
+
+
+
+# import sys
+# N,M = map(int,sys.stdin.readline().split())
+
+# S_a = set(sys.stdin.readline().strip() for _ in range(N))
+# S_b = set(sys.stdin.readline().strip() for _ in range(M))
+
+# intersection = S_a & S_b
+# L = list(intersection)
+# L.sort()
+
+# print(len(L))
+# for i in L:
+#   print(i)
+
+
+
+
+# import sys
+# N,M = map(int,sys.stdin.readline().split())
+
+# Set_A = set(map(int,sys.stdin.readline().split()))
+# Set_B = set(map(int,sys.stdin.readline().split()))
+
+# print(len(Set_A ^ Set_B))
+# # print(len((Set_A - Set_B) | (Set_B-Set_A)))
+
+
+
+
 import sys
 
-N = int(sys.stdin.readline())
-Set_a = set(map(int,sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-L = list(map(int,sys.stdin.readline().split()))
-Set_b = set(L)
+str = sys.stdin.readline().strip()
+S = set()
 
-intersection = Set_a & Set_b
+for i in range(1,len(str)+1):
+  for j in range(len(str)+1-i):
+    temp = str[j:i+j]
+    S.add(temp)
 
-for i in range(M):
-  if L[i] in intersection:
-    print(1)
-  else:
-    print(0)
+print(len(S))
