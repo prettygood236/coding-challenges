f0b2ecf287507c6fa05aaabb3b3b5f44b072f502
prettygood236 <prettygood236@gmail.com>
2022-09-21 15:22:34 +0900

    Done with all geometry questions :)

--- __geometry__.py
+++ __geometry__.py
@@ -74,23 +74,74 @@
 
 
 
-# * 수학적 지식이 부족해서 어려웠다.
-import sys
-T = int(sys.stdin.readline())
+# # * 수학적 지식이 부족해서 어려웠다.
+# import sys
+# T = int(sys.stdin.readline())
 
-for _ in range(T):
-  xo,yo,ro,xt,yt,rt = map(int,sys.stdin.readline().split())
-  # 두 원 중심 사이의 거리
-  distance = (abs(xo-xt)**2 + abs(yo-yt)**2) ** 0.5
-  # 두 원이 완전히 포개어 지는 경우. (교점이 무한대)
-  if xo == xt and yo == yt and ro == rt:
-    print(-1)
-  # 두 원이 두 점에서 만나는 경우.
-  elif abs(ro - rt) < distance < ro + rt :
-    print(2)
-  # 두 원이 내접하거나 외접하는 경우. (교점이 하나)
-  elif abs(ro - rt) == distance or ro + rt == distance:
-    print(1)
-  # 두 원의 교점이 없는 경우. 
-  elif distance < abs(ro - rt) or ro + rt < distance :
-    print(0)
+# for _ in range(T):
+#   xo,yo,ro,xt,yt,rt = map(int,sys.stdin.readline().split())
+#   # 두 원 중심 사이의 거리
+#   distance = (abs(xo-xt)**2 + abs(yo-yt)**2) ** 0.5
+#   # 두 원이 완전히 포개어 지는 경우. (교점이 무한대)
+#   if xo == xt and yo == yt and ro == rt:
+#     print(-1)
+#   # 두 원이 두 점에서 만나는 경우.
+#   elif abs(ro - rt) < distance < ro + rt :
+#     print(2)
+#   # 두 원이 내접하거나 외접하는 경우. (교점이 하나)
+#   elif abs(ro - rt) == distance or ro + rt == distance:
+#     print(1)
+#   # 두 원의 교점이 없는 경우. 
+#   elif distance < abs(ro - rt) or ro + rt < distance :
+#     print(0)
+
+
+
+
+# 출발점 도착점을 포함하고 있는 원의 개수를 구하면 됨.
+# * 원의 방정식 -> (x-a)**2 + (y-b)**2 = r**2 
+# * 따라서 (X'-a)**2 + (Y'-b)**2 <= r**2 를 만족시키는 (X',Y')는 원의 경계 또는 원 안에 포함된다!. 
+# import sys 
+# T = int(sys.stdin.readline())
+# L = []
+
+# for _ in range(T):
+#   c = 0
+#   x1, y1, x2, y2 = map(int,sys.stdin.readline().split())
+#   n = int(sys.stdin.readline())
+#   for _ in range(n):
+#     cx, cy, r = map(int,sys.stdin.readline().split()) 
+#     start = (x1-cx)**2 + (y1-cy)**2
+#     end = (x2-cx)**2 + (y2-cy)**2
+#     if(start <= r**2 and end > r**2) or (start > r**2 and end <= r**2): 
+#       c += 1
+#   L.append(c)
+
+# print(*L,sep='\n')
+
+
+
+
+
+# 세 구역으로 나눠서 생각해보자 
+# 직사각형 구역. (X <= x and Y <= y) and (X+W >= x and Y+H >= y) 이면 링크 안 또는 경계에 있다.
+# 왼쪽 반원 구역.  R = H/2 , (X-x)**2 + ((Y+R)-y)**2  <= R**2  
+# 오른쪽 반원 구역.  R = H/2 , ((X+W)-x)**2 + ((Y+R)-y)**2  <= R**2  
+import sys
+W,H,X,Y,P = map(int,sys.stdin.readline().split())
+R = H/2
+count = 0
+
+for _ in range(P):
+  x,y = map(int,sys.stdin.readline().split())
+  if (X <= x and Y <= y) and (X+W >= x and Y+H >= y):
+    count += 1
+    continue
+  if ((X-x)**2 + ((Y+R)-y)**2  <= R**2):
+    count += 1
+    continue
+  if (((X+W)-x)**2 + ((Y+R)-y)**2  <= R**2):
+    count += 1
+    continue
+
+print(count)
