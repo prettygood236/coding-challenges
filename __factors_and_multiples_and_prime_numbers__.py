9cbb2630879c2b654de9d5e096cf59dac4db8ce9
prettygood236 <prettygood236@gmail.com>
2023-03-26 21:08:17 +0900

    done with some problems

--- __factors_and_multiples_and_prime_numbers__.py
+++ __factors_and_multiples_and_prime_numbers__.py
@@ -22,6 +22,96 @@
 # print(max(L)*min(L))
 
 
+# 하나씩 나눠보며 구하기
+# import sys
+
+# n = int(sys.stdin.readline())
+# l = list(map(int,sys.stdin.readline().split()))
+
+# r = 0
+# for i in l:
+#   if i == 1 :
+#     continue
+#   if i == 2 :
+#     r += 1
+#     continue
+
+#   d = 1 
+#   while True:
+#     d += 1
+#     if i % d == 0 and i != d:
+#       break
+#     elif i == d:
+#       r += 1
+#       break
+  
+# print(r)
+  
+
+
+# Sieve of Eratosthenes
+# def prime_list(n):
+#   sieve = [True]*n
+
+#   m = int(n ** 0.5)
+#   for i in range(2, m+1):
+#     if sieve[i] == True:
+#       for j in range(i+i,n,i):
+#         sieve[j] = False
+
+#   return [i for i in range(2,n) if sieve[i] == True]
+
+# print(prime_list(62)) # 62보다 작은 자연수 중 소수의 리스트 출력.
+
+
+
+# M <= 요 사이 소수 구하기 <= N
+# import sys
+
+# def prime_list(n):
+#   sieve = [True]*n
+
+#   m = int(n ** 0.5)
+#   for i in range(2,m+1):
+#     if sieve[i] == True:
+#       for j in range(i+i,n,i):
+#         sieve[j] = False
+  
+#   return [i for i in range(2,n) if sieve[i] == True]
+       
+# M = int(sys.stdin.readline())
+# N = int(sys.stdin.readline())
+
+# Ml = prime_list(M)
+# Nl = prime_list(N+1)
+
+# complement = list(set(Nl)-set(Ml))
+
+# if len(complement) == 0:
+#   print(-1)
+# else:
+#   print(sum(complement))
+#   print(complement[0])
+
+
+
+#. <소인수 분해>
+
+# def prime_list(n):
+  
+#   m = int(n ** 0.5)
+#   sieve = [True]*(m+1)
+
+#   mm = int(m ** 0.5)
+#   for i in range(2,mm+1):
+#     if sieve[i] == True:
+#       for j in range(i+i,m,i):
+#         sieve[j] = False
+
+#   return [i for i in range(2,m) if sieve[i] == True] 
+  
+  
+
     
 
 
@@ -99,6 +189,29 @@
 
 
 
+#. <LCM>
+import sys
+c,d = map(int,sys.stdin.readline().split())
+# breakpoint()
+a = max(c,d)
+b = min(c,d)
+
+r = -1
+
+while True:
+  r = a%b
+  if r == 0:
+    break
+  a = b
+  b = r
+    
+g = b
+a = max(c,d)
+b = min(c,d)
+
+lcm = a*b//g 
+
+print(lcm)
 
 
 #. a = MQ + R, b = MQ' + R c = MQ'' + R
@@ -276,31 +389,31 @@
 
 
 
-import sys
+# import sys
 
-n,m = map(int,sys.stdin.readline().split())
+# n,m = map(int,sys.stdin.readline().split())
 
-# #. RecursionError: maximum recursion depth exceeded 
-# def factorial(x):
-#   if x > 1 :
-#     return x * factorial(x-1)
-#   else :
-    # return 1
+# # #. RecursionError: maximum recursion depth exceeded 
+# # def factorial(x):
+# #   if x > 1 :
+# #     return x * factorial(x-1)
+# #   else :
+#     # return 1
 
-def two_count(x):
-  count = 0
-  while x != 0:
-    x //= 2
-    count += x
-  return count
+# def two_count(x):
+#   count = 0
+#   while x != 0:
+#     x //= 2
+#     count += x
+#   return count
 
-def five_count(x):
-  count = 0
-  while x != 0:
-    x //= 5
-    count += x
-  return count
+# def five_count(x):
+#   count = 0
+#   while x != 0:
+#     x //= 5
+#     count += x
+#   return count
 
-#. n! / ((n-m)! * m!)
+# #. n! / ((n-m)! * m!)
 
-print(min(two_count(n) - (two_count(n-m) + two_count(m)), five_count(n) - (five_count(n-m) + five_count(m))))
+# print(min(two_count(n) - (two_count(n-m) + two_count(m)), five_count(n) - (five_count(n-m) + five_count(m))))
---
+++ __geometry_rectangle_and_triangle__.py
@@ -0,0 +1,72 @@
+
+#. <Rectangle>
+# a = int(input())
+# b = int(input())
+# print(a*b)
+
+
+#. <Escape from the rectangle>
+# import sys
+
+# x,y,w,h = map(int,sys.stdin.readline().split())
+
+# print(min(w-x,h-y,x,y))
+
+
+#. <CETVRTA>
+# import sys
+# cnt_x = dict()
+# cnt_y = dict()
+
+# for _ in range(3):
+#   x,y = map(int,sys.stdin.readline().split())
+#   if x in cnt_x:
+#     cnt_x[x] += 1
+#   else:
+#     cnt_x[x] = 1
+
+#   if y in cnt_y:
+#     cnt_y[y] += 1
+#   else:
+#     cnt_y[y] = 1
+  
+# print(min(cnt_x,key=cnt_x.get),min(cnt_y,key=cnt_y.get))
+
+
+
+#. <Math is a physical education subject>
+# print(int(input())*4)
+
+
+#. BAEKJOON 9063. <Ground>
+import sys
+n = int(sys.stdin.readline())
+min_x, min_y = 10000,10000
+max_x, max_y = -10000,-10000
+for _ in range(n):
+  x,y = map(int,sys.stdin.readline().split())
+  min_x = min(min_x,x)
+  min_y = min(min_y,y)
+  max_x = max(max_x,x)
+  max_y = max(max_y,y)
+
+w = abs(max_x - min_x)
+h = abs(max_y - min_y)
+result = w*h
+
+print(w*h)
+
+
+
+#. BAEKJOON 9063. <Triangle Times>
+a = int(input())
+b = int(input())
+c = int(input())
+if a==60 and a==b and b==c:
+  print('Equilateral')
+elif a+b+c==180 and (a==b or b==c or a==c):
+  print('Isosceles')
+elif a+b+c==180 and (a!=b and b!=c and a!=c):
+  print('Scalene')
+elif a+b+c!=180:
+  print('Error')
