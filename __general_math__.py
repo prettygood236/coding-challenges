f44a411b44328c2c0366eb42cd82b8a0046e871e
prettygood236 <prettygood236@gmail.com>
2023-04-05 17:00:44 +0900

    done with some problems

--- __general_math__.py
+++ __general_math__.py
@@ -1,2 +1,35 @@
 
-#. BAEKJOON 2745. <Number system conversion>
+#. BAEKJOON 2745. <Number system conversion 1>
+# import sys
+# n,b = sys.stdin.readline().split()
+# b = int(b)
+# result = 0
+# c = len(n)-1
+
+# for i in n:
+#   temp = int(i,36)
+#   result += temp*(b**c)
+#   c-=1
+
+# print(result)
+
+
+
+#. BAEKJOON 11005. <Number system conversion 2>
+# import sys
+# n,b = map(int,sys.stdin.readline().split())
+
+# digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"  
+# result = ""
+
+# while n > 0:
+#   remainder = n % b
+#   result = digits[remainder] + result
+#   n //= b  
+
+# print(result)  
+  
+
+
+#. BAEKJOON 2720. <Quick Change>
+
--- __test__.py
+++ __test__.py
@@ -1,12 +1,4 @@
-from datetime import datetime, timedelta
 
-a = "2016-09-15 01:00:07.000 2s"
+a = 'zzzzz'
 
-
-datetime_dt = datetime.strptime(' '.join(a.split()[0:2]), '%Y-%m-%d %H:%M:%S.%f')
-time_diff = timedelta(seconds=-float(a.split()[-1][:-1])+0.001)
-result_dt = datetime_dt + time_diff
-result_str = result_dt.strftime('%Y-%m-%d %H:%M:%S.%f')
-a = result_str
-
-breakpoint()
+print(int(a[0],36))
