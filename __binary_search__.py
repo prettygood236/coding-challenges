37258ffc0f2029b974003d3f7a47c86bffdba6e7
prettygood236 <prettygood236@gmail.com>
2023-06-02 21:16:56 +0900

    Done with some binary search problems

--- __binary_search__.py
+++ __binary_search__.py
@@ -72,28 +72,74 @@
 
 
 #. BAEKJOON 2805. <EKO>
-import sys
-N,M = map(int,sys.stdin.readline().split())
-L = list(map(int,sys.stdin.readline().split()))
-L.sort(reverse=True)
+# import sys
+# N,M = map(int,sys.stdin.readline().split())
+# L = list(map(int,sys.stdin.readline().split()))
+# L.sort(reverse=True)
 
-start_num = 0
-end_num = L[0]
+# start_num = 0
+# end_num = L[0]
 
-while start_num <= end_num:
-    mid_num = (start_num + end_num) // 2
+# while start_num <= end_num:
+#     mid_num = (start_num + end_num) // 2
 
-    required_wood = 0
-    for i in L:
-        if i <= mid_num:
-            break
-        required_wood += (i - mid_num)
+#     required_wood = 0
+#     for i in L:
+#         if i <= mid_num:
+#             break
+#         required_wood += (i - mid_num)
 
-    if required_wood >= M:
-        start_num = mid_num + 1
-    else:
-        end_num = mid_num - 1
+#     if required_wood >= M:
+#         start_num = mid_num + 1
+#     else:
+#         end_num = mid_num - 1
 
-print(end_num)
+# print(end_num)
 
+
+
+
+
+#. BAEKJOON 2110. <Aggressive cows>
+# def check_distance(stalls,min_distance,cows):
+#     cows_count = 1
+#     last_stall = stalls[0]
+
+#     for stall in stalls[1:]:
+#         if stall - last_stall >= min_distance:
+#             cows_count += 1
+#             last_stall = stall
+#             if cows_count == cows:
+#                 return True
+#     return False
+
+# def largest_minimum_distance(N,C,stalls):
+#     # breakpoint()
+#     stalls.sort()
+#     #. 범위를 설정하고 이분탐색한다!! 
+#     #. largest_minimum_distance의 가능한 범위는 1 ~ stalls[-1] - stalls[0]
+#     left = 1
+#     right  = stalls[-1] - stalls[0]
+#     ans = 0
+
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if check_distance(stalls,mid,C):
+#             ans = mid
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+    
+#     return ans
+
+# N,C = map(int,input().split())
+# # N : stall의 개수 C: 소의 마리수
+# stalls = [int(input()) for _ in range(N)]
+# # stalls : 축사 위치의 집합
+# print(largest_minimum_distance(N,C,stalls))
+
+
+
+
+#. BAEKJOON 2110. <Aggressive cows>
 
