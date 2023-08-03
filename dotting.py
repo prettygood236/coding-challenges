8c2ec0114b3317b40c261711651806526633cc14
prettygood236 <prettygood236@gmail.com>
2023-08-04 00:43:02 +0900

    Done with dotting

--- dotting.py
+++ dotting.py
@@ -1,14 +1,8 @@
-def solution(k, d):
-    list = []
-    for i in range(d+1):
-        for j in range(d+1):
-            if (i**2)+(j**2) <= d**2:
-                list.append((i,j))
-    answer = 0
-    return list,len(list)
+import math
 
-
-print(solution(1,6))
-print(solution(1,7))
-print(solution(1,8))
-print(solution(1,9))
+def solution(k,d):
+    result = 0
+    for x in range(0,d+1,k):
+        y = math.floor(((d**2)-(x**2))**0.5)
+        result += y//k + 1
+    return result
