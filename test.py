61a03e0db2f378cb49ea58e0a6f6932893562219
prettygood236 <prettygood236@gmail.com>
2023-08-05 16:48:04 +0900

    Done with Hash problem 1

---
+++ test.py
@@ -0,0 +1,21 @@
+# def solution(participant, completion):
+#     # breakpoint()
+#     participant.sort()
+#     completion.sort()
+#     answer = ''
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             answer = participant[i]
+#             break
+#     if answer == '':
+#         answer = participant[-1]
+#     return answer
+
+import collections
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    breakpoint()
+    return
+
+print(solution(["leo", "kiki", "eden"],["eden", "kiki"]))   
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"],["josipa", "filipa", "marina", "nikola"]))   
