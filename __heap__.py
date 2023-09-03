b4973aa60619b32756c24ba2c6da0b9986edf40e
prettygood236 <prettygood236@gmail.com>
2023-09-03 16:56:50 +0900

    done with 'Double Priority Queue' Problem. (learned the method using both min-heap and max-heap along with the lazy deletion technique)

--- __heap__.py
+++ __heap__.py
@@ -150,35 +150,136 @@
 # 2ms 시점에 6ms 걸리는 작업 요청이 들어옵니다.
 
 
-import heapq 
+# import heapq 
 
-def solution(jobs):
-    answer = 0
-    current_time = 0
-    length = len(jobs)
-    heap = []
+# def solution(jobs):
+#     answer = 0
+#     current_time = 0
+#     length = len(jobs)
+#     heap = []
     
-    #. 요청시간 -> 작업시간 순으로 정렬
-    jobs.sort()
+#     #. 요청시간 -> 작업시간 순으로 정렬
+#     jobs.sort()
     
-    while jobs or heap:
-        #. 현재 시간 내에 요청된 모든 작업을 힙에 추가
-        while jobs and jobs[0][0] <= current_time:
-            request_time, work_time = heapq.heappop(jobs)
-            #. 작업시간이 짧은 순서대로 힙에 삽입 (작업시간, 요청시간) 순서로 삽입 (모듈에 의해 구현된 힙에 넣는 것만으로 정렬이 자동으로 된다)
-            heapq.heappush(heap, (work_time, request_time))
+#     while jobs or heap:
+#         #. 현재 시간 내에 요청된 모든 작업을 힙에 추가
+#         while jobs and jobs[0][0] <= current_time:
+#             request_time, work_time = heapq.heappop(jobs)
+#             #. 작업시간이 짧은 순서대로 힙에 삽입 (작업시간, 요청시간) 순서로 삽입 (모듈에 의해 구현된 힙에 넣는 것만으로 정렬이 자동으로 된다)
+#             heapq.heappush(heap, (work_time, request_time))
         
-        if heap:
-            #. 가장 빨리 끝나는 작업부터 처리 
-            work_time, request_time = heapq.heappop(heap)
-            current_time += work_time
-            answer += current_time - request_time
+#         if heap:
+#             #. 가장 빨리 끝나는 작업부터 처리 
+#             work_time, request_time = heapq.heappop(heap)
+#             current_time += work_time
+#             answer += current_time - request_time
 
-        else: 
-          #. 현재 진행 가능한 작업이 없으면 다음으로 가장 빨리 들어오는 job의 시작시점으로 이동  
-          current_time = jobs[0][0]
+#         else: 
+#           #. 현재 진행 가능한 작업이 없으면 다음으로 가장 빨리 들어오는 job의 시작시점으로 이동  
+#           current_time = jobs[0][0]
             
-    return answer // length
+#     return answer // length
 
-jobs = [[0, 3], [1, 9], [2, 6]] 		
-print(solution(jobs)) #9
+# jobs = [[0, 3], [1, 9], [2, 6]] 		
+# print(solution(jobs)) #9
+
+
+
+
+#- <Double Priority Queue>
+# 문제 설명
+# 이중 우선순위 큐는 다음 연산을 할 수 있는 자료구조를 말합니다.
+
+# 명령어	수신 탑(높이)
+# I 숫자	큐에 주어진 숫자를 삽입합니다.
+# D 1	큐에서 최댓값을 삭제합니다.
+# D -1	큐에서 최솟값을 삭제합니다.
+# 이중 우선순위 큐가 할 연산 operations가 매개변수로 주어질 때, 모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return 하도록 solution 함수를 구현해주세요.
+
+# 제한사항
+# operations는 길이가 1 이상 1,000,000 이하인 문자열 배열입니다.
+# operations의 원소는 큐가 수행할 연산을 나타냅니다.
+# 원소는 “명령어 데이터” 형식으로 주어집니다.- 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제합니다.
+# 빈 큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시합니다.
+# 입출력 예
+# operations	return
+# ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]	[0,0]
+# ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]	[333, -45]
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 16과 -5643을 삽입합니다.
+# 최솟값을 삭제합니다. -5643이 삭제되고 16이 남아있습니다.
+# 최댓값을 삭제합니다. 16이 삭제되고 이중 우선순위 큐는 비어있습니다.
+# 우선순위 큐가 비어있으므로 최댓값 삭제 연산이 무시됩니다.
+# 123을 삽입합니다.
+# 최솟값을 삭제합니다. 123이 삭제되고 이중 우선순위 큐는 비어있습니다.
+# 따라서 [0, 0]을 반환합니다.
+
+# 입출력 예 #2
+
+# -45와 653을 삽입후 최댓값(653)을 삭제합니다. -45가 남아있습니다.
+# -642, 45, 97을 삽입 후 최댓값(97), 최솟값(-642)을 삭제합니다. -45와 45가 남아있습니다.
+# 333을 삽입합니다.
+# 이중 우선순위 큐에 -45, 45, 333이 남아있으므로, [333, -45]를 반환합니다.
+
+
+
+#. heapq implements a min heap, so there is no guarantee that the last value of the heap is the largest.
+#. Therefore, we optimize by using both min-heap and max-heap, along with the Lazy Deletion technique. 
+#* Lazy deletion : A method where we don't immediately perform the actual delete operation, but instead store items to be deleted separately. 
+#. Then, when we're looking up the maximum or minimum value of the heap and one of them applies, that's when we actually delete it."
+#. Time complexity : O(NlogN)
+
+import heapq
+
+def solution(operations):
+    min_heap = []
+    max_heap = []
+    alive_num = {}
+
+    for op in operations:
+        operation, number = op.split(' ')
+        number = int(number)
+
+        if operation == 'I':
+            heapq.heappush(min_heap, number)
+            #. Multiply by -1 to implement a 'max heap'
+            heapq.heappush(max_heap, -number)
+            alive_num[number] = alive_num.get(number, 0) + 1
+        if operation == 'D':
+            if not min_heap or not max_heap:  
+                continue
+
+            if number > 0:
+                while max_heap and -max_heap[0] not in alive_num:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    del_val = -heapq.heappop(max_heap)
+                    alive_num[del_val] -= 1
+                    if alive_num[del_val] == 0:
+                        del alive_num[del_val]
+            else:
+                while min_heap and min_heap[0] not in alive_num:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    del_val = heapq.heappop(min_heap)
+                    alive_num[del_val] -= 1
+                    if alive_num[del_val] == 0: 
+                        del alive_num[del_val]
+
+    while max_heap and -max_heap[0] not in alive_num: 
+        heapq.heappop(max_heap)
+
+    while min_heap and min_heap[0] not in alive_num: 
+        heapq.heappop(min_heap)
+
+    return [-(heapq.heappop(max_heap)) ,heapq.heappop(min_heap)] if max_heap else [0,0]
+
+
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]	
+print(solution(operations)) # [0,0]
+operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+print(solution(operations)) # [333, -45]
+operations = ["I 2","I 4","D -1", "I 1", "D 1"]
+print(solution(operations)) # [1,1]
