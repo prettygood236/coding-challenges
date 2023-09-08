bb6992e6123f86d2a0849e1c52deab149b6a6bd2
prettygood236 <prettygood236@gmail.com>
2023-09-08 15:30:06 +0900

    Done with Programmers brute force problem - <Mock exam> Learned about 1. List comprehension, 2. method of using enumerate

--- __brute_force__.py
+++ __brute_force__.py
@@ -162,7 +162,7 @@
 
 
 
-#. <Minimum Rectangle>
+#- <Minimum Rectangle>
 #. 'Always think simply first!'
 # 문제 설명
 # 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다. 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다. 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
@@ -200,18 +200,66 @@
 # 명함들을 적절히 회전시켜 겹쳤을 때, 모든 명함을 포함하는 가장 작은 지갑의 크기는 133(=19 x 7)입니다.
 
 
-def solution(sizes):
-    big,small = 0,0
-    for size in sizes:
-        if max(size) > big:
-            big = max(size)
-        if min(size) > small:
-            small = min(size)
-    return big*small
+# def solution(sizes):
+#     big,small = 0,0
+#     for size in sizes:
+#         if max(size) > big:
+#             big = max(size)
+#         if min(size) > small:
+#             small = min(size)
+#     return big*small
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]	
-print(solution(sizes)) #4000
-sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	
-print(solution(sizes)) #120
-sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]	
-print(solution(sizes)) #133
+# sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]	
+# print(solution(sizes)) #4000
+# sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	
+# print(solution(sizes)) #120
+# sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]	
+# print(solution(sizes)) #133
+
+
+
+
+#- <Mock exam>
+#. 'Learned about 1. List comprehension, 2. method of using enumerate'
+# 문제 설명
+# 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+# 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+# 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+# 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+# 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+# 제한 조건
+# 시험은 최대 10,000 문제로 구성되어있습니다.
+# 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+# 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+# 입출력 예
+# answers	return
+# [1,2,3,4,5]	[1]
+# [1,3,2,4,2]	[1,2,3]
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 수포자 1은 모든 문제를 맞혔습니다.
+# 수포자 2는 모든 문제를 틀렸습니다.
+# 수포자 3은 모든 문제를 틀렸습니다.
+# 따라서 가장 문제를 많이 맞힌 사람은 수포자 1입니다.
+
+# 입출력 예 #2
+
+# 모든 사람이 2문제씩을 맞췄습니다.
+
+# def solution(answers):
+#     math_dropout = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+#     result = [0]*3
+#     for i in range(len(answers)):
+#         for j in range(3):
+#             if answers[i] == math_dropout[j][i%len(math_dropout[j])]:
+#                 result[j] += 1      
+#     return [i+1 for i,v in enumerate(result) if v == max(result)]
+
+# answers = [1,2,3,4,5]
+# print(solution(answers)) #[1]
+# answers = [1,3,2,4,2]	
+# print(solution(answers)) #[1,2,3]
