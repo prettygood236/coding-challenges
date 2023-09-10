b6da975f2c761e51f871231bb0440a3de9566fce
prettygood236 <prettygood236@gmail.com>
2023-09-10 16:18:40 +0900

    Done with Programmers problem - <Fatigue> Calculate the time complexity first! The total number of cases is O(n* .n) 40320*8. Isnt this within the time complexity\?

--- __brute_force__.py
+++ __brute_force__.py
@@ -319,9 +319,6 @@
 #- <Carpet>
 #. If we check the range of variables and it does not exceed the time complexity, a brute force search can be advantageous.
 #. (In fact, this problem can be solved by setting up two equations for length and area and finding the solution to the quadratic equation.)
-
-#. 변수의 범위를 확인해 시간복잡도를 초과하지 않을 경우에는 완전탐색이다!.
-#. (사실 이 문제는 길이, 넓이의 두식을 세워 이차방정식의 해로 구할 수 있다.)
 # 문제 설명
 # Leo는 카펫을 사러 갔다가 아래 그림과 같이 중앙에는 노란색으로 칠해져 있고 테두리 1줄은 갈색으로 칠해져 있는 격자 모양 카펫을 봤습니다.
 
@@ -342,22 +339,106 @@
 # 24	24	[8, 6]
 # 출처
 
-def solution(brown, yellow):
-    temp = 2
-    while True:
-        # breakpoint()
-        expected_w = (brown - temp) // 2
-        expected_h = (temp//2) + 2
-        if brown + yellow == expected_w*expected_h:
-            return [expected_w,expected_h]
-        temp += 2
+# def solution(brown, yellow):
+#     temp = 2
+#     while True:
+#         # breakpoint()
+#         expected_w = (brown - temp) // 2
+#         expected_h = (temp//2) + 2
+#         if brown + yellow == expected_w*expected_h:
+#             return [expected_w,expected_h]
+#         temp += 2
 
-brown = 10
-yellow = 2
-print(solution(brown, yellow)) #[4,3]
-brown = 8
-yellow = 1
-print(solution(brown, yellow)) #[3,3]
-brown = 24
-yellow = 24
-print(solution(brown, yellow)) #[8,6]
+# brown = 10
+# yellow = 2
+# print(solution(brown, yellow)) #[4,3]
+# brown = 8
+# yellow = 1
+# print(solution(brown, yellow)) #[3,3]
+# brown = 24
+# yellow = 24
+# print(solution(brown, yellow)) #[8,6]
+
+
+
+
+#- <Fatigue>
+#. Calculate the time complexity first. 
+#. The total number of cases is O(n! * n) : 40320*8. Isn't this within the time complexity?
+# 문제 설명
+# XX게임에는 피로도 시스템(0 이상의 정수로 표현합니다)이 있으며, 일정 피로도를 사용해서 던전을 탐험할 수 있습니다. 이때, 각 던전마다 탐험을 시작하기 위해 필요한 "최소 필요 피로도"와 던전 탐험을 마쳤을 때 소모되는 "소모 피로도"가 있습니다. "최소 필요 피로도"는 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도를 나타내며, "소모 피로도"는 던전을 탐험한 후 소모되는 피로도를 나타냅니다. 예를 들어 "최소 필요 피로도"가 80, "소모 피로도"가 20인 던전을 탐험하기 위해서는 유저의 현재 남은 피로도는 80 이상 이어야 하며, 던전을 탐험한 후에는 피로도 20이 소모됩니다.
+
+# 이 게임에는 하루에 한 번씩 탐험할 수 있는 던전이 여러개 있는데, 한 유저가 오늘 이 던전들을 최대한 많이 탐험하려 합니다. 유저의 현재 피로도 k와 각 던전별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열 dungeons 가 매개변수로 주어질 때, 유저가 탐험할수 있는 최대 던전 수를 return 하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# k는 1 이상 5,000 이하인 자연수입니다.
+# dungeons의 세로(행) 길이(즉, 던전의 개수)는 1 이상 8 이하입니다.
+# dungeons의 가로(열) 길이는 2 입니다.
+# dungeons의 각 행은 각 던전의 ["최소 필요 피로도", "소모 피로도"] 입니다.
+# "최소 필요 피로도"는 항상 "소모 피로도"보다 크거나 같습니다.
+# "최소 필요 피로도"와 "소모 피로도"는 1 이상 1,000 이하인 자연수입니다.
+# 서로 다른 던전의 ["최소 필요 피로도", "소모 피로도"]가 서로 같을 수 있습니다.
+# 입출력 예
+# k	dungeons	result
+# 80	[[80,20],[50,40],[30,10]]	3
+# 입출력 예 설명
+# 현재 피로도는 80입니다.
+
+# 만약, 첫 번째 → 두 번째 → 세 번째 던전 순서로 탐험한다면
+
+# 현재 피로도는 80이며, 첫 번째 던전을 돌기위해 필요한 "최소 필요 피로도" 또한 80이므로, 첫 번째 던전을 탐험할 수 있습니다. 첫 번째 던전의 "소모 피로도"는 20이므로, 던전을 탐험한 후 남은 피로도는 60입니다.
+# 남은 피로도는 60이며, 두 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 50이므로, 두 번째 던전을 탐험할 수 있습니다. 두 번째 던전의 "소모 피로도"는 40이므로, 던전을 탐험한 후 남은 피로도는 20입니다.
+# 남은 피로도는 20이며, 세 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 30입니다. 따라서 세 번째 던전은 탐험할 수 없습니다.
+# 만약, 첫 번째 → 세 번째 → 두 번째 던전 순서로 탐험한다면
+
+# 현재 피로도는 80이며, 첫 번째 던전을 돌기위해 필요한 "최소 필요 피로도" 또한 80이므로, 첫 번째 던전을 탐험할 수 있습니다. 첫 번째 던전의 "소모 피로도"는 20이므로, 던전을 탐험한 후 남은 피로도는 60입니다.
+# 남은 피로도는 60이며, 세 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 30이므로, 세 번째 던전을 탐험할 수 있습니다. 세 번째 던전의 "소모 피로도"는 10이므로, 던전을 탐험한 후 남은 피로도는 50입니다.
+# 남은 피로도는 50이며, 두 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 50이므로, 두 번째 던전을 탐험할 수 있습니다. 두 번째 던전의 "소모 피로도"는 40이므로, 던전을 탐험한 후 남은 피로도는 10입니다.
+# 따라서 이 경우 세 던전을 모두 탐험할 수 있으며, 유저가 탐험할 수 있는 최대 던전 수는 3입니다.
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    dungeons_permutations = list(permutations(dungeons,len(dungeons)))
+    answer = [0]*len(dungeons_permutations)
+    # breakpoint()
+    for i,v in enumerate(dungeons_permutations):
+        temp = k
+        for m,u in v:
+            if temp < m:
+                break
+            else:
+                answer[i] += 1
+                temp -= u
+    return max(answer)
+
+#. Using DFS
+# answer = 0
+# N = 0
+# visited = []
+
+# def dfs(k, cnt, dungeons):
+#     global answer
+#     if cnt > answer:
+#         answer = cnt
+
+#     for j in range(N):
+#         if k >= dungeons[j][0] and not visited[j]:
+#             visited[j] = 1
+#             dfs(k - dungeons[j][1], cnt + 1, dungeons)
+#             visited[j] = 0
+
+# def solution(k, dungeons):
+#     global N, visited
+#     N = len(dungeons)
+#     visited = [0] * N
+#     dfs(k, 0, dungeons)
+#     return answer
+
+#? Using recursion
+# solution = lambda k, d: max([solution(k - u, d[:i] + d[i+1:]) + 1 for i, (m, u) in enumerate(d) if k >= m] or [0])
+
+
+k = 80	
+dungeons = [[80,20],[50,40],[30,10]]	
+print(solution(k,dungeons)) #3 
