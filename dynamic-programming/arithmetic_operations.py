d6db8327a9b5c10c3e82d85410565755dd80e964
prettygood236 <prettygood236@gmail.com>
2023-10-14 19:22:23 +0900

    Done with Dynamic Programming Arithmetic Operations

---
+++ dynamic-programming/arithmetic_operations.py
@@ -0,0 +1,75 @@
+# ---
+# title: Arithmetic Operations
+# tags: [CodingChallenges, Python, DynamicProgramming]
+# created: '2023-10-14'
+# ---
+
+# ## Problem Description
+
+# In arithmetic operations, addition (+) follows the associative law, but subtraction (-) does not. For example, the expression 1 - 5 - 3 can yield different results depending on the order of operations:
+
+# ((1 - 5) - 3) = -7
+# (1 - (5 - 3)) = -1
+# As shown above, subtraction can produce different results depending on the order of operations. 
+
+# Another example is the expression 1 - 3 + 5 - 8 which can yield five different results depending on operation orders:
+
+# (((1 - 3) + 5) - 8) = -5
+# ((1 - (3 + 5)) -8 ) =-15 
+# (1- ((3+5)-8))=1 
+# (1-(3+(5-8)))=1 
+# ((1-3)+(5-8))=-5 
+# The different operation orders for this expression give [-15,-5,-5,1,11] and among these maximum is `11`.
+
+# Given an array `arr` containing strings of numbers and two operators "+" and "-", return the maximum value that can be obtained by calculating in all possible orders.
+
+# ## Solution
+
+# This problem can be solved using dynamic programming. The idea is to calculate maximum and minimum values for all possible sets of numbers by considering each possible location of operators.
+
+# ```python
+def solution(arr):
+    # n is the number of digits in the given array.
+    n = (len(arr) + 1) // 2
+
+    # dp_max and dp_min are tables (2D arrays) to store the maximum and minimum values, respectively. 
+    # Initialize with very small (-infinity) and very large (+infinity) values.
+    dp_max = [[-float('inf')] * n for _ in range(n)]
+    dp_min = [[float('inf')] * n for _ in range(n)]
+
+    # Set each digit's own maximum and minimum values to itself.
+    for i in range(n):
+        dp_max[i][i] = dp_min[i][i] = int(arr[i*2])
+
+    # This part performs a loop over all possible sections (sets of numbers).
+    for cnt in range(2, n+1):
+        # 'cnt' represents the number of digits included in each section.
+        # Starting from '2', it considers all possible sections starting from those made up of two digits.
+        
+        for start in range(n-cnt+1):
+            # 'start' is the starting point of each section. 
+            # For example, if the array is [1,'+',2,'-',3], possible starting points are first element '1', third element '+', fifth element '2', etc., which correspond to indices 0, 2, 4 respectively.
+            
+            end = start + cnt - 1
+            # 'end' is the end point of each section calculated based on the defined 'start' and number of digits included ('cnt').
+
+            # For '+' case: Based on where the '+' operator is located, divide into left and right sections,
+            # The sum of either maximum or minimum value from each section becomes that section's maximum or minimum value.
+            # For '-' case: Based on where the '-' operator is located, divide into left and right sections,
+            # Subtracting right section's minimum value from left section's maximum value becomes that section's maximum value,
+            # Subtracting right section's maximum value from left section's minimum value becomes that section’s minimum value.
+            for i in range(start, end):
+                if arr[i*2+1] == '+':
+                    dp_max[start][end] = max(dp_max[start][end], dp_max[start][i] + dp_max[i+1][end])
+                    dp_min[start][end] = min(dp_min[start][end], dp_min[start][i] + dp_min[i+1][end])
+                else:
+                    dp_max[start][end] = max(dp_max[start][end], dp_max[start][i] - dp_min[i+1][end])
+                    dp_min[start][end] = min(dp_min[start][end], dp_min[start][i] - dp_max[i+1][end])
+
+    # Finally, 'dp_max[0][-1]' represents among calculation results of largest possible scope containing all numbers. 
+    return int(dp_max[0][-1])
+
+arr = ["1", "-", "3", "+", "5", "-", "8"]	
+print(solution(arr)) # 1
+arr = ["5", "-", "3", "+", "1", "+", "2", "-", "4"]	
+print(solution(arr)) # 3
--- test.py
+++ test.py
@@ -1,57 +1,3 @@
-# 사칙연산
-# 문제 설명
-# 사칙연산에서 더하기(+)는 결합법칙이 성립하지만, 빼기(-)는 결합법칙이 성립하지 않습니다.
-# 예를 들어 식 1 - 5 - 3은 연산 순서에 따라 다음과 같이 다른 결과를 가집니다.
-
-# ((1 - 5) - 3) = -7
-# (1 - (5 - 3)) = -1
-# 위 예시와 같이 뺄셈은 연산 순서에 따라 그 결과가 바뀔 수 있습니다.
-# 또 다른 예로 식 1 - 3 + 5 - 8은 연산 순서에 따라 다음과 같이 5가지 결과가 나옵니다.
-
-# (((1 - 3) + 5) - 8) = -5
-# ((1 - (3 + 5)) - 8) = -15
-# (1 - ((3 + 5) - 8)) = 1
-# (1 - (3 + (5 - 8))) = 1
-# ((1 - 3) + (5 - 8)) = -5
-# 위와 같이 서로 다른 연산 순서의 계산 결과는 [-15, -5, -5, 1, 1]이 되며, 이중 최댓값은 1입니다.
-# 문자열 형태의 숫자와, 더하기 기호("+"), 뺄셈 기호("-")가 들어있는 배열 arr가 매개변수로 주어질 때, 서로 다른 연산순서의 계산 결과 중 최댓값을 return 하도록 solution 함수를 완성해 주세요.
-
-# 제한 사항
-# arr는 두 연산자 "+", "-" 와 숫자가 들어있는 배열이며, 길이는 3 이상 201 이하 입니다.
-# arr의 길이는 항상 홀수입니다.
-# arr에 들어있는 숫자의 개수는 2개 이상 101개 이하이며, 연산자의 개수는 (숫자의 개수) -1 입니다.
-# 숫자는 1 이상 1,000 이하의 자연수가 문자열 형태로 들어있습니다.. (ex : "456")
-# 배열의 첫 번째 원소와 마지막 원소는 반드시 숫자이며, 숫자와 연산자가 항상 번갈아가며 들어있습니다.
-
-# def solution(arr):
-#     num_list = list(map(int,arr[::2]))
-#     op_list = arr[1::2] + ['']
-#     n = len(num_list)
-#     dp = [[0]*n for _ in range(n)]
-
-#     for i in range(n):
-#         dp[i][i] = num_list[i]
-
-#     for i in range(len(num_list)-1, -1, -1):
-#         for j in range(i+1, len(num_list)):
-#             if op_list[j-1] == '+':
-#                 dp[i][j] = dp[i][j-1] + num_list[j]
-#             else:
-#                 dp[i][j] = max(dp[k+1][j]-dp[i][k]+(num_list[i]*2) for k in range(i,j))
-                
-#     breakpoint()
-#     return dp[0][-1]
-
-
-# arr = ["1", "-", "3", "+", "5", "-", "8"]	
-# print(solution(arr)) # 1
-# ["5", "-", "3", "+", "1", "+", "2", "-", "4"]	
-# print(solution(arr)) # 3
-
-
-
-
-
 
 
 
@@ -76,35 +22,35 @@
 # 집과 학교가 물에 잠긴 경우는 입력으로 주어지지 않습니다.
 
 
-def solution(m, n, puddles):
-    dp = [[0]*m for _ in range(n)]
-    mod = 1000000007
+# def solution(m, n, puddles):
+#     dp = [[0]*m for _ in range(n)]
+#     mod = 1000000007
 
-    for i,j in puddles:
-        dp[i-1][j-1] = 'puddle'
+#     for i,j in puddles:
+#         dp[i-1][j-1] = 'puddle'
 
-    for i in range(1,n):
-        if dp[i][0] == 'puddle':
-            break
-        dp[i][0] = 1
+#     for i in range(1,n):
+#         if dp[i][0] == 'puddle':
+#             break
+#         dp[i][0] = 1
 
-    for j in range(1,m):
-        if dp[0][j] == 'puddle':
-            break
-        dp[0][j] = 1
+#     for j in range(1,m):
+#         if dp[0][j] == 'puddle':
+#             break
+#         dp[0][j] = 1
 
-    for i in range(1,n):
-        for j in range(1,m):
-            if dp[i][j] == 'puddle' or (dp[i-1][j] == 'puddle' and dp[i][j-1] == 'puddle'):
-                continue
-            elif dp[i][j-1] == 'puddle':
-                dp[i][j] = dp[i-1][j] % mod
-            elif dp[i-1][j] == 'puddle':
-                dp[i][j] = dp[i][j-1] % mod
-            else:
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod
+#     for i in range(1,n):
+#         for j in range(1,m):
+#             if dp[i][j] == 'puddle' or (dp[i-1][j] == 'puddle' and dp[i][j-1] == 'puddle'):
+#                 continue
+#             elif dp[i][j-1] == 'puddle':
+#                 dp[i][j] = dp[i-1][j] % mod
+#             elif dp[i-1][j] == 'puddle':
+#                 dp[i][j] = dp[i][j-1] % mod
+#             else:
+#                 dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod
     
-    return dp[-1][-1]
+#     return dp[-1][-1]
 
-m = 4; n = 3; puddles= [[1,3],[2, 2]]
-print(solution(m, n, puddles)) #4
+# m = 4; n = 3; puddles= [[1,3],[2, 2]]
+# print(solution(m, n, puddles)) #4
