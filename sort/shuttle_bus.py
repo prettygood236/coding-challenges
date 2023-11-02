12727bd0770f30dc7cbd48359cfec11785417c75
prettygood236 <prettygood236@gmail.com>
2023-11-02 19:16:42 +0900

    Organizing some files

--- sort/shuttle_bus.py
+++ sort/shuttle_bus.py
@@ -1,3 +1,26 @@
+# ---
+# title: Shuttle Bus
+# tags: [CodingChallenges, Python, Sorting]
+# created: '2023-01-22'
+# ---
+
+# ## Problem Description
+
+# A shuttle bus operates n times from 09:00. It departs every t minutes and can carry up to m people. Given a list of crew arrival times in `timetable`, determine the latest time you should arrive to catch the shuttle bus. If there is not enough room on the bus for all the crew, you should arrive earlier to ensure you get a seat.
+
+# ## Constraints
+
+# - The shuttle bus operates n times (1 ≤ n ≤ 10).
+# - The bus departs every t minutes (1 ≤ t ≤ 60).
+# - The bus can carry up to m people (1 ≤ m ≤ 45).
+# - Each time in `timetable` is given in the format "HH:MM".
+# - `timetable` contains at least 1 and no more than 2,000 times.
+
+# ## Solution
+
+# The problem can be solved by sorting the timetable in descending order and then filling the bus for each departure time. If the bus is full or there are no more crew, the bus departs. If there is room on the last bus, you should arrive at the last departure time. If the last bus is full, you should arrive one minute before the last crew member that got a seat.
+
+# ```python
 def solution(n,t,m,timetable):
   answer = ''
   for i in range(len(timetable)):
